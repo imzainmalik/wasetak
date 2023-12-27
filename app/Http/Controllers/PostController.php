15bccb24d0689f04760c5732ed2512da70406c1f
Junaid Shoaib <developerlegendesk@gmail.com>
2023-12-27 23:05:38 +0500

    Home page Done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Bookmark;
+use App\Models\LikedReply;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Post;
 use App\Models\PostView;
+use App\Models\PostLike;
+use App\Models\PostReply;
 
 class PostController extends Controller
 {
@@ -13,15 +18,75 @@ class PostController extends Controller
         if(!$post){
             return abort(404);
         }
-
+        $like_check = null;
+        $bookmark = null;
+        $comment_like_check = null;
         if(auth()->check()){
             $post_view = PostView::updateOrCreate([
                 'user_id'   => auth()->user()->id,
                 'post_id'     => $post->id,
             ]);
-            
+            $like_check = PostLike::where('user_id' , auth()->user()->id)->where('post_id', $post->id)->first();
+            $bookmark = Bookmark::where('user_id' , auth()->user()->id)->where('post_id', $post->id)->first();
+          
         }
+
+        // @dd($comment_like_check);
         return view('User.post_detail', get_defined_vars());
+    }
+
+
+
+
+    public function user_like_post($post_id){
+
+        $like = PostLike::where('user_id', auth()->user()->id)->where('post_id', $post_id)->first();
+        if($like){
+            $like->delete();
+            return response()->json(['status'=>0]);
+        }else{
+            $like = new PostLike();
+             $like->user_id = auth()->user()->id;
+             $like->post_id = $post_id;
+             $like->save();
+
+         }
+        
+        return response()->json(['status'=>1]);
+    }
+
+
+    public function user_like_post_comment($reply_id){
+
+        $reply_like = LikedReply::where('user_id', auth()->user()->id)->where('reply_id', $reply_id)->first();
+        if($reply_like){
+            $reply_like->delete();
+            return response()->json(['status'=>0]);
+        }else{
+             $reply_like = new likedReply();
+             $reply_like->user_id = auth()->user()->id;
+             $reply_like->reply_id = $reply_id;
+             $reply_like->save();
+
+         }
+        
+        return response()->json(['status'=>1]);
+    }
+    
+    public function user_bookmark_post($post_id){
+
+        $bookmark = Bookmark::where('user_id', auth()->user()->id)->where('post_id', $post_id)->first();
+        if($bookmark){
+            $bookmark->delete();
+            return response()->json(['status'=>0]);
+        }else{
+             $bookmark = new Bookmark();
+             $bookmark->user_id = auth()->user()->id;
+             $bookmark->post_id = $post_id;
+             $bookmark->save();
+         }
+        
+        return response()->json(['status'=>1]);
     }
 
     
--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -63,8 +63,8 @@
                 <div class="col-md-6">
                     <h2>Buy, Sell, & Trade Virtual Properties</h2>
                     <p class="para">wasetak is a trusted middleman service dedicated to offering our users the safest way to buy, sell, or trade items and services of virtual nature.</p>
-                    <a href="#" class="theme-btn1"><img src="{{asset('user_asset/img/card11.png')}}" alt=""> Free Calculator </a>
-                    <a href="#" class="theme-btn2"><img src="{{asset('user_asset/img/card10.png')}}" alt=""> Start ticket</a>
+                    <a href="{{route('user.fee_calculator')}}" class="theme-btn1"><img src="{{asset('user_asset/img/card11.png')}}" alt=""> Free Calculator </a>
+                    <a href="{{route('user.start_checkout')}}" class="theme-btn2"><img src="{{asset('user_asset/img/card10.png')}}" alt=""> Start ticket</a>
                 </div>
             </div>
         </div>
@@ -86,17 +86,58 @@
                                     @endif
                                     <li data-targetit="box-3">Top</li>
                                     <li data-targetit="box-4">Categories</li>
+                                    @if(auth()->check())
                                     <li data-targetit="box-5">My Post</li>
+                                    @endif
                                     <li data-targetit="box-6">Hot</li>
                                 </ul>
                             </div>
                             <div class="col-md-5 text-e">
                                 <ul class="drop">
                                     <li class="dropdown drop1">
-                                        <a href="#" class="dropbtn1"><i class="fas fa-sort-down"></i> All Categories</a>
+                                        <a href="#" class="dropbtn1"><i class="fas fa-sort-down"></i> 
+                                        @if($p_name != '' && $c_name != '')
+                                        {{$p_name . ' - ' . $c_name}}
+                                        @elseif($p_name != '')
+                                        {{ $p_name }}
+                                        @else
+                                        All Categories
+                                        @endif
+                                        </a>
                                         <div class="dropdown-content">
-                                            <input type="text" placeholder="Search">
+                                            {{-- <input type="text" placeholder="Search"> --}}
                                             <div class="cate-list">
+                                                <a href="{{route('user.index')}}">
+                                                    <h5><span class="box1"></span> All Categories</h5>
+                                                    <p class="para"></p>
+                                                </a>
+                                            </div>
+                                            <div class="bor-line"></div>
+                                                @foreach ($all_categories as $item)
+                                                    <div class="cate-list">
+                                                        <a href="{{route('user.index')}}/?p_id={{$item['id']}}">
+                                                            <h5><span class="box1"style="background-color: {{ $item['color'] }}"></span> {{ $item['name'] }}</h5>
+                                                            <p class="para">{{$item['description']}}</p>
+                                                        </a>
+                                                    </div>
+                                                    <div class="bor-line"></div>
+                                                        @if (isset($item[0]) && is_array($item[0]))
+                                                            @foreach ($item as $child)
+                                                                @if (is_array($child))
+                                                                    <div class="cate-list">
+                                                                        <a href="{{route('user.index')}}/?p_id={{$item['id']}}&c_id={{$child['child_id']}}">
+                                                                            <h5><span class="box5" style="background-color: {{ $item['color'] }}"></span> {{ $item['name'] }} </h5>
+                                                                            <h5><span class="box1" style="background-color: {{ $child['child_color'] }}" ></span>  {{ $child['child_name'] }} </h5>
+                                                                            <p class="para">{{$child['child_description']}}</p>
+                                                                        </a>
+                                                                    </div>
+                                                                    <div class="bor-line"></div>
+                                                                @endif
+                                                            @endforeach
+                                                        @endif
+                                                @endforeach
+                                            
+                                            {{-- <div class="cate-list">
                                                 <a href="create-topics.php">
                                                     <h5><span class="box1"></span> Buyer Request </h5>
                                                     <p class="para">Are you looking to buy a Facebook/Twitter/Youtube/Pintrest,
@@ -142,7 +183,7 @@
                                                         or any other peoperty we don’t have listed on Wasetak ? Quite
                                                         often, many of wasetak seller’s don’t list everything they have</p>
                                                 </a>
-                                            </div>
+                                            </div> --}}
                                         </div>
                                     </li>
                                     <li class="dropdown">
@@ -238,7 +279,8 @@
                             </div>
                         </div>
                         @endforelse
-                        {!! $posts->links() !!}
+                        {{-- {!! $posts->links() !!} --}}
+                        {!! $posts->appends(request()->query())->links() !!}
                     </div>
                 </div>
                 @if(auth()->check())
@@ -389,7 +431,7 @@
                             </div>
                         </div>
                         @endforelse
-                        {!! $top_posts->links() !!}
+                        {!! $top_posts->appends(request()->query())->links() !!}
                         {{-- <div class="col-md-12">
                             <div class="boxed-wrap">
                                 <div class="row align-items-center">
@@ -689,6 +731,82 @@
                     </div>
                 </div>
 
+                @if(auth()->check())
+                <div class="box-5 {{request()->get('my_post') ? 'showfirst' : ''}}">
+                    <div class="col-md-12 mar">
+                        <div class="row">
+                            <div class="col-md-8 col-3">
+                                <h5>Topic</h5>
+                            </div>
+                            <div class="col-md-4 col-9 text-e">
+                                <h5>Replies</h5>
+                                <h5>Views</h5>
+                                <h5>Activity</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row rowgap">
+                        @forelse ($my_post as $post)
+                        <div class="col-md-12">
+                            <div class="boxed-wrap">
+                                <div class="row align-items-center">
+                                    <div class="col-md-7">
+                                        <a href="{{route('user.post_detail',[$post->id])}}"><h4>{{$post->title}}</h4></a>
+                                        <ul class="links">
+                                            <li><span class="span">{{$post->getCatInfo ? $post->getCatInfo->name : ''}}</span><span class="box1" style="background-color: {{$post->getCatInfo->color}} !important"></span></li>
+                                            @if ($post->getSubCatInfo)
+
+                                            <li><span class="span"> {{ $post->getSubCatInfo->name }}</span><span class="box2" style="background-color: {{$post->getSubCatInfo->color}} !important"></span></li>
+                                            @endif
+                                        </ul>
+                                        {{-- <ul class="cate">
+                                            <li class="active"><a href="#">Featured</a></li>
+                                            <li><a href="#">Name-Change</a></li>
+                                            <li><a href="#">Urban-Service</a></li>
+                                            <li><a href="#">Username-Claim</a></li>
+                                            <li><a href="#">Verification</a></li>
+                                        </ul> --}}
+                                    </div>
+                                    <div class="col-md-5 pe-md-0">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-3 p-md-0">
+                                                {{-- <img src="{{asset('user_asset/img/card13.png')}}" class="img1" alt=""> --}}
+                                            </div>
+                                            <div class="col-md-4">
+                                                <a href="{{route('user.post_detail',[$post->id])}}" class="rep">
+                                                    <img src="{{asset('user_asset/img/card14.png')}}" alt="">
+                                                    <h5>{{ $post->getPostReplies->where('is_active',1)->count() }} Replies</h5>
+                                                </a>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <a href="#" class="rep">
+                                                    <img src="{{asset('user_asset/img/card12.png')}}" alt="">
+                                                    <h5>{{ $post->getPostViews->count() }} views</h5>
+                                                </a>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <h5>{{$post->created_at->diffForHumans()}}</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @empty
+                        <div class="col-md-12">
+                            <div class="boxed-wrap">
+                                <div class="row align-items-center">
+                                    <div class="col-md-12">
+                                        <h4>No Record Found</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforelse
+                        {!! $posts->appends(request()->query())->links() !!}
+                    </div>
+                </div>
+                @endif
                 <div class="box-6 {{request()->get('featured_page') ? 'showfirst' : ''}}">
                     <div class="col-md-12 mar">
                         <div class="row">
@@ -760,7 +878,7 @@
                             </div>
                         </div>
                         @endforelse
-                        {!! $posts->links() !!}
+                        {!! $posts->appends(request()->query())->links() !!}
                     </div>
                 </div>
             </div>
